149bb31aa0d0dd721f551e16c5799b92a5d13878
Ygor Frazão <ygorluizfrazao@gmail.com>
2023-04-27 13:00:40 -0300

    Working on the project card Layout

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ $(document).ready((e)=>{
     $('.slide-show').slick({
         autoplay: true,
         infinite: true,
+        dots: true,
         speed: 500,
         fade: true,
         cssEase: 'linear',

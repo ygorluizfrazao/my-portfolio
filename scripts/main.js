07e2e4cedfa5dbc5914a23f3ee8d26214923a2da
Ygor Frazão <ygorluizfrazao@gmail.com>
2023-04-27 10:08:52 -0300

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+
+$(document).ready((e)=>{
+
+    $('.slide-show').slick({
+        autoplay: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        arrows : false
+      });
+
+})
